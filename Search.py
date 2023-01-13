84d909158c76c50bebef39e7172ecda881ed0d6d
Павел Берия <beriya2004@mail.ru>
2023-01-13 19:04:03 +0300

    Added colorama

--- Search.py
+++ Search.py
@@ -1,5 +1,11 @@
 import psycopg2
 from dbconfig import *
+from colorama import Fore
+
+White = Fore.WHITE
+Yellow = Fore.YELLOW
+Green = Fore.GREEN
+Red = Fore.RED
 
 db = psycopg2.connect(
     host="localhost",
@@ -9,32 +15,32 @@ db = psycopg2.connect(
 )
 
 cur = db.cursor()
-print("Search system (type !number to choose from results or !exit to exit)")
+print(f"{Green}Search {White}system (type !number to choose from results or !exit to exit)\n")
 
 while True:
-    srch = input("Search => ")
+    srch = input(f"{Green}Search {White}=> ")
     choice = 0
     if srch[0] == '!':
         if srch[1:] == "exit":
-            print("Exitting...")
+            print(Yellow + "Exitting...")
             break
         if choice - 1 < len(data):
             choice = int(srch[1:])
             print(
-                f"Price for the {data[choice-1][0]} is \xa3{data[choice-1][1]}")
+                f"{White}Price for the {Green}{data[choice-1][0]} {White}is {Green}\xa3{data[choice-1][1]}")
             continue
         else:
-            print("There is no such element in the database")
+            print(Red + "There is no such element in the database")
 
     cur.execute(
         f"SELECT * FROM data WHERE name LIKE '{srch.capitalize()}%' ORDER BY name")
     data = cur.fetchall()
 
     if len(data) == 0:
-        print("There is no such element in the database")
+        print(Red + "There is no such element in the database")
         continue
 
-    print(f"\nTotal occurrences: {len(data)}")
+    print(f"\n{White}Total occurrences: {Green}{len(data)}{White}")
 
     for i in range(len(data)):
         row = data[i]
